aba669c6623110fcd5fb84ff17d555db4775f5ae
manese1 <richiekmanese@gmail.com>
2026-02-16 13:25:40 +0300

    initial commit

---
+++ JijiCart/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for JijiCart project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JijiCart.settings')
+
+application = get_asgi_application()
---
+++ cartapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from cartapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', views.home),
+    path('gallery/', views.gallery),
+]
