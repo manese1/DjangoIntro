0dc08636506d17e3fc28540db8a65ef8efb6ad3f
manese1 <richiekmanese@gmail.com>
2026-02-19 22:29:36 +0300

    new commit

--- cartapp/urls.py
+++ cartapp/urls.py
@@ -5,6 +5,7 @@ from cartapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home),
-    path('gallery/', views.gallery),
+    path('home/', views.home, name = 'home'),
+    path('gallery/', views.gallery, name = 'gallery'),
+    path('services/', views.services, name = 'services')
 ]
--- cartapp/views.py
+++ cartapp/views.py
@@ -5,3 +5,5 @@ def home(request):
     return render(request, 'index.html')
 def gallery(request):
     return render(request, 'gallery.html')
+def services(request):
+    return render(request, 'services.html')
