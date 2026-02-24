7f127dff99dd14a9bc80015b6a2c87a2e54ea336
manese1 <richiekmanese@gmail.com>
2026-02-24 21:29:41 +0300

    new commit

--- cartapp/models.py
+++ cartapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    productname = models.CharField(max_length = 100)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+    description = models.TextField()
+    def __str__(self):
+        return self.productname
